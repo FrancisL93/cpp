66e498090f3930f3b875e1d726d839f5438f2c94
Francis Lahoud <flahoud@c3r9p1.42quebec.com>
2022-10-07 11:30:15 -0400

    c00-ex00 completed

--- cpp00/ex00/Megaphone.cpp
+++ cpp00/ex00/Megaphone.cpp
@@ -6,16 +6,23 @@
 /*   By: flahoud <flahoud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 15:27:37 by flahoud           #+#    #+#             */
-/*   Updated: 2022/10/07 11:02:45 by flahoud          ###   ########.fr       */
+/*   Updated: 2022/10/07 11:29:29 by flahoud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <iomanip>
-#include <cstdlib>
+#include <cstring>
 
 int	main(int argc, char **argv)
 {
-	std::cout << "argc == " << argc << '\n';
-	(void) argv;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int i = 1; argv[i]; i++) {
+		for (size_t j = 0; j < strlen(argv[i]); j++) {
+			argv[i][j] = toupper(argv[i][j]);
+			std::cout << argv[i][j];
+		}
+	}
+	std::cout << "\n";
+	return 0;
 }
