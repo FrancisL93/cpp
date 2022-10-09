469009ca607a087bde935e330d6eac7028cdc29c
Francis Lahoud <francislahoud@MacBook-Pro-de-Francis.local>
2022-10-09 16:42:19 -0400

    add Makefile to push modules

---
+++ cpp00/ex01/main.cpp
@@ -0,0 +1,42 @@
+#include "phonebook.hpp"
+
+void	display_intro(void){
+	std::cout << "\n\e[1;32mWelcome to the PhoneBook!\e[0m\n\n";
+	std::cout << "Enter ADD to save anew contact\n";
+	std::cout << "Enter SEARCH to display a specific contact\n";
+	std::cout << "Enter EXIT to quit the PhoneBook\n\n";
+}
+
+void	search(void){
+	return ;
+}
+
+void	add(void){
+	return ;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc == 1 && argv)
+    {
+		display_intro();	
+		std::string input;
+		std::cout << "Type your command: ";
+		std::getline(std::cin, input);
+		while (input != "EXIT") {
+			if (std::cin.eof())
+				return (0);
+			if(input == "ADD")
+				add();
+			else if (input == "SEARCH")
+				search();
+			else
+				std::cout << "\e[1;31mCommand invalid!\e[0m\nRe-";
+			std::cout << "Type your command: ";
+			std::getline(std::cin, input);
+		}
+    }
+	else
+		std::cout << "ERROR: Execute ./phonebook without arguments!\n";
+	return (0);
+}
---
+++ cpp00/ex01/phonebook.hpp
@@ -0,0 +1,8 @@
+#ifndef CPP00_EX00_PHONEBOOK_H
+#define CPP00_EX00_PHONEBOOK_H
+
+# include <iomanip>
+# include <iostream>
+# include <cstring>
+
+#endif
